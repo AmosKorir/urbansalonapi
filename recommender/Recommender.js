0cf3c6b57188bf88f294f613e38e564db05f92e5
Kmos <amoskrr@gmail.com>
2019-07-03 07:37:05 +0300

    order graph

--- recommender/Recommender.js
+++ recommender/Recommender.js
@@ -56,8 +56,9 @@ const runSession = function runSession(cypher, params) {
 
 const orderGraph = function insertOrderGraph(order) {
 	console.log(order);
+	console.log(order.serviceid);
 	var cypher = 'MATCH (a:customer),(b:service) WHERE a.customerid={customerid} AND b.serviceid={serviceid} CREATE (a)-[r:BOOKED]->(b)';
-	var params = { serviceid: order.serviceid.toString(), customerid: order.customerid.toString() };
+	var params = { serviceid: order.serviceid, customerid: order.customerid.toString() };
 	runSession(cypher, params);
 };
 
