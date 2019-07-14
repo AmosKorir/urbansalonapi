7f4c5c3a14fe40074ca5d3cb1d4e14248029dc0d
Kmos <amoskrr@gmail.com>
2019-07-14 14:34:31 +0300

    added the analytic

--- controllers/AnalyticController.js
+++ controllers/AnalyticController.js
@@ -4,15 +4,15 @@ const router = express.Router();
 const handler = require('../utils/Errorhandler');
 
 //get booking for the past seven dates
-router.post('/seven', (req, res) => {
-	var startDate = req.startDate;
-	var endDate = req.endDate;
+router.get('/seven', (req, res) => {
+	var startDate = getStartDate(7);
+    var endDate = new Date();
 	dateAnalytic(
 		startDate,
 		endDate,
 		result => {
-            res.json(result);
-        },
+			res.json(result);
+		},
 		error => {
 			handler.handleError(res, 500, error.message);
 		}
@@ -34,3 +34,8 @@ const dateAnalytic = function getDateAnalytic(startdate, endDate, callback, erro
 			errorCallback(error);
 		});
 };
+
+function getStartDate(dateRange) {
+	var currentDate = new Date();
+	return currentDate.getDate() - 10;
+}
