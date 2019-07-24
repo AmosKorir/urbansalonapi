0d4b49dc945c076d3fab9bb9fdd70e0e3a3cefd6
Kmos <amoskrr@gmail.com>
2019-07-24 19:05:36 +0300

    fixing average

--- controllers/ServiceController.js
+++ controllers/ServiceController.js
@@ -130,7 +130,7 @@ router.get('/all', (req, res) => {
 				attributes: [[Sequelize.fn('SUM', Sequelize.col('rating')), 'total']],
 			},
 		],
-		group: ['rating.serviceid,service.serviceid'],
+		group: ['rate.serviceid,service.serviceid'],
 		
 	})
 		.then(response => {
