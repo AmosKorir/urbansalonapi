5925f48ab51ba0db15831ded910c0bccbf6c52a9
Kmos <amoskrr@gmail.com>
2019-07-24 16:54:28 +0300

    removing salon graph

--- models/Relationship.js
+++ models/Relationship.js
@@ -11,6 +11,7 @@ Order.belongsTo(Service, { foreignKey: 'serviceid' });
 Service.hasMany(Order, { foreignKey: 'serviceid' });
 
 Service.hasMany(Rating,{as:'rating',foreignKey:'serviceid'});
+Rating.belongsTo(Service,{foreignKey:'serviceid'});
 
 Order.belongsTo(Customer, { foreignKey: 'customerid' });
 Customer.hasMany(Order, { as: 'orders', foreignKey: 'customerid' });
